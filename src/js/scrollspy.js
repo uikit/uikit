90d4095277b646af4e58f719b69abcc1f71fefd1
Artur Heinze <artur@yootheme.com>
2013-08-13 17:15:12 +0200

    scrollspy refactored

--- src/js/scrollspy.js
+++ src/js/scrollspy.js
@@ -6,6 +6,8 @@
 
         ScrollSpy   = function(element, options) {
 
+            this.options = $.extend({}, this.options, options);
+
             var $this = this, inviewstate, initinview,
                 fn = function(){
 
@@ -15,19 +17,20 @@
 
                         if(!initinview) {
                             $this.element.addClass($this.options.initcls);
+                            $this.offset = $this.element.offset();
                             initinview = true;
 
                             $this.element.trigger("uk-scrollspy-init");
                         }
 
-                        $this.element.addClass($this.options.cls);
+                        $this.element.addClass("uk-scrollspy-inview").addClass($this.options.cls).width();
                         inviewstate = true;
 
                         $this.element.trigger("uk-scrollspy-inview");
                     }
 
-                    if (!inview && inviewstate) {
-                        $this.element.removeClass($this.options.cls);
+                    if (!inview && inviewstate && !$this.options.initial) {
+                        $this.element.removeClass("uk-scrollspy-inview").removeClass($this.options.cls);
                         inviewstate = false;
 
                         $this.element.trigger("uk-scrollspy-outview");
@@ -36,7 +39,7 @@
 
             this.element = $(element);
 
-            $win.on("scroll", fn).on("resize orientationchange", UI.Utils.debounce(fn, 150));
+            $win.on("scroll", fn).on("resize orientationchange", UI.Utils.debounce(fn, 50));
 
             fn();
         };
@@ -47,7 +50,8 @@
             "cls": "uk-scrollspy-inview",
             "initcls": "uk-scrollspy-init-inview",
             "topoffset": 0,
-            "leftoffset": 0
+            "leftoffset": 0,
+            "initial": false
         }
 
     });
@@ -63,7 +67,7 @@
             return false;
         }
 
-        var window_left = $win.scrollLeft(), window_top = $win.scrollTop(), offset = $element.offset(), left = offset.left, top = offset.top;
+        var window_left = $win.scrollLeft(), window_top = $win.scrollTop(), offset = obj.offset || $element.offset(), left = offset.left, top = offset.top;
 
         if (top + $element.height() >= window_top && top - options.topoffset <= window_top + $win.height() &&
             left + $element.width() >= window_left && left - options.leftoffset <= window_left + $win.width()) {
