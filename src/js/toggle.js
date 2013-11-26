ff1a5aa40248e21ec5d0f2a471715a92e767bce0
Artur Heinze <artur@yootheme.com>
2013-11-26 16:02:52 +0100

    toggle cleanup

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -7,9 +7,9 @@
 
         if($element.data("toggle")) return;
 
-        this.options  = $.extend({}, this.options, options);
+        this.options  = $.extend({}, Toggle.defaults, options);
         this.totoggle = this.options.toggle ? $(this.options.toggle):[];
-        this.element  = $element.on("click", function(e) {
+        this.element  = $element.on(UI.Utils.events.click, function(e) {
             e.preventDefault();
             $this.toggle();
         });
@@ -19,27 +19,27 @@
 
     $.extend(Toggle.prototype, {
 
-        options: {
-            toggle: false,
-            toggleclass: 'uk-hidden'
-        },
-
         toggle: function() {
 
             if(!this.totoggle.length) return;
 
-            this.totoggle.toggleClass(this.options.toggleclass);
+            this.totoggle.toggleClass(this.options.cls);
         }
     });
 
+    Toggle.defaults = {
+        toggle: false,
+        cls: 'uk-hidden'
+    };
+
     UI["toggle"] = Toggle;
 
-    $(document).on("click.toggle.uikit", "[data-uk-toggle]", function(e) {
+    $(document).on(UI.Utils.events.click+".toggle.uikit", "[data-uk-toggle]", function(e) {
         var ele = $(this);
 
         if (!ele.data("toggle")) {
            var obj = new Toggle(ele, UI.Utils.options(ele.attr("data-uk-toggle")));
-           ele.trigger("click");
+           ele.trigger(UI.Utils.events.click);
         }
     });
 
