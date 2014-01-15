68c047c9c6dfa73e2671e46bbf5088b1f1ef30cf
Artur Heinze <artur@yootheme.com>
2014-01-15 13:48:48 +0100

    data-uk-grid-match reverted

--- src/js/grid.js
+++ src/js/grid.js
@@ -2,21 +2,83 @@
 
     "use strict";
 
-    var GridMatch = function(element, options) {
+    var win = $(window), event = 'resize orientationchange';
 
-        var $element = $(element);
+    var GridMatchHeight = function(element, options) {
+
+        var $this = this, $element = $(element);
 
         if($element.data("gridMatchHeight")) return;
 
-        this.options  = $.extend({}, GridMatch.defaults, options);
+        this.options  = $.extend({}, GridMatchHeight.defaults, options);
 
-        var matchHeight = new UI.matchHeight($element, this.options);
+        this.element  = $element;
+        this.columns  = this.element.children();
+        this.elements = this.options.target ? this.element.find(this.options.target) : this.columns;
 
-        $element.data("gridMatchHeight", matchHeight);
+        if (!this.columns.length) return;
+
+        win.on(event, (function() {
+            var fn = function() {
+                $this.match();
+            };
+
+            $(function() {
+                fn();
+                win.on("load", fn);
+            });
+
+            return UI.Utils.debounce(fn, 150);
+        })());
+
+        $(document).on("uk-domready", function(e) {
+            $this.columns  = $this.element.children();
+            $this.elements = $this.options.target ? $this.element.find($this.options.target) : $this.columns;
+            $this.match();
+        });
+
+        this.element.data("gridMatchHeight", this);
     };
 
+    $.extend(GridMatchHeight.prototype, {
 
-    GridMatch.defaults = {
+        match: function() {
+
+            this.revert();
+
+            var firstvisible = this.columns.filter(":visible:first");
+
+            if (!firstvisible.length) return;
+
+            var stacked = Math.ceil(100 * parseFloat(firstvisible.css('width')) / parseFloat(firstvisible.parent().css('width'))) >= 100 ? true : false,
+                max     = 0,
+                $this   = this;
+
+            if (stacked) return;
+
+            this.elements.each(function() {
+                max = Math.max(max, $(this).outerHeight());
+            }).each(function(i) {
+
+                var element   = $(this),
+                    boxheight = element.css("box-sizing") == "border-box" ? "outerHeight" : "height",
+                    box       = $this.columns.eq(i),
+                    height    = (element.height() + (max - box[boxheight]()));
+
+                element.css('min-height', height + 'px');
+            });
+
+            return this;
+        },
+
+        revert: function() {
+            this.elements.css('min-height', '');
+            return this;
+        }
+
+    });
+
+    GridMatchHeight.defaults = {
         "target": false
     };
 
@@ -37,7 +99,7 @@
         cls: 'uk-grid-margin'
     };
 
-    UI["gridMatch"]  = GridMatch;
+    UI["gridMatchHeight"]  = GridMatchHeight;
     UI["gridMargin"] = GridMargin;
 
     // init code
@@ -46,7 +108,7 @@
             var grid = $(this), obj;
 
             if (grid.is("[data-uk-grid-match]") && !grid.data("gridMatchHeight")) {
-                obj = new GridMatch(grid, UI.Utils.options(grid.attr("data-uk-grid-match")));
+                obj = new GridMatchHeight(grid, UI.Utils.options(grid.attr("data-uk-grid-match")));
             }
 
             if (grid.is("[data-uk-grid-margin]") && !grid.data("gridMargin")) {
--- src/js/utility.js
+++ src/js/utility.js
@@ -4,84 +4,6 @@
 
     var win = $(window), event = 'resize orientationchange';
 
-    var MatchHeight = function(element, options) {
-
-        var $this = this, $element = $(element);
-
-        if($element.data("matchHeight")) return;
-
-        this.options  = $.extend({}, MatchHeight.defaults, options);
-
-        this.element  = $element;
-        this.columns  = this.element.children();
-        this.elements = this.options.target ? this.element.find(this.options.target) : this.columns;
-
-        if (!this.columns.length) return;
-
-        win.on(event, (function() {
-            var fn = function() {
-                $this.match();
-            };
-
-            $(function() {
-                fn();
-                win.on("load", fn);
-            });
-
-            return UI.Utils.debounce(fn, 150);
-        })());
-
-        $(document).on("uk-domready", function(e) {
-            $this.columns  = $this.element.children();
-            $this.elements = $this.options.target ? $this.element.find($this.options.target) : $this.columns;
-            $this.match();
-        });
-
-        this.element.data("matchHeight", this);
-    };
-
-    $.extend(MatchHeight.prototype, {
-
-        match: function() {
-
-            this.revert();
-
-            var firstvisible = this.columns.filter(":visible:first");
-
-            if (!firstvisible.length) return;
-
-            var stacked = Math.ceil(100 * parseFloat(firstvisible.css('width')) / parseFloat(firstvisible.parent().css('width'))) >= 100 ? true : false,
-                max     = 0,
-                $this   = this;
-
-            if (stacked) return;
-
-            this.elements.each(function() {
-                max = Math.max(max, $(this).outerHeight());
-            }).each(function(i) {
-
-                var element   = $(this),
-                    boxheight = element.css("box-sizing") == "border-box" ? "outerHeight" : "height",
-                    box       = $this.columns.eq(i),
-                    height    = (element.height() + (max - box[boxheight]()));
-
-                element.css('min-height', height + 'px');
-            });
-
-            return this;
-        },
-
-        revert: function() {
-            this.elements.css('min-height', '');
-            return this;
-        }
-
-    });
-
-    MatchHeight.defaults = {
-        "target": false
-    };
-
     var StackMargin = function(element, options) {
 
         var $this = this, $element = $(element);
@@ -161,19 +83,14 @@
     };
 
 
-    UI["matchHeight"] = MatchHeight;
     UI["stackMargin"] = StackMargin;
 
     // init code
     $(document).on("uk-domready", function(e) {
-        $("[data-uk-match-height],[data-uk-margin]").each(function() {
+        $("[data-uk-margin]").each(function() {
             var ele = $(this), obj;
 
-            if (ele.is("[data-uk-match-height]") && !ele.data("matchHeight")) {
-                obj = new MatchHeight(ele, UI.Utils.options(ele.attr("data-uk-match-height")));
-            }
-
-            if (ele.is("[data-uk-margin]") && !ele.data("stackMargin")) {
+            if (!ele.data("stackMargin")) {
                 obj = new StackMargin(ele, UI.Utils.options(ele.attr("data-uk-margin")));
             }
         });
