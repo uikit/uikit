964cffe19a231285c54d62b0f1c3216e137afbe4
Artur Heinze <artur@yootheme.com>
2014-03-04 10:12:36 +0100

    core.js amd config fix

--- src/js/core.js
+++ src/js/core.js
@@ -7,7 +7,7 @@
 
             uikit.load = function(res, req, onload, config) {
 
-                var resources = res.split(','), load = [], i, base = (config.uikit && config.uikit.base ? config.uikit.base : "").replace(/\/+$/g, "");
+                var resources = res.split(','), load = [], i, base = (config.config && config.config.uikit && config.config.uikit.base ? config.config.uikit.base : "").replace(/\/+$/g, "");
 
                 if (!base) {
                     throw new Error( "Please define base path to uikit in the requirejs config." );
