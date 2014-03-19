de5007c3b1d12b06d68862b4d173c925136d5335
Artur Heinze <artur@yootheme.com>
2014-03-19 09:45:05 +0100

    template helper updated

--- src/js/core.js
+++ src/js/core.js
@@ -199,7 +199,7 @@
 
     UI.Utils.template = function(str, data) {
 
-        var tokens = str.replace(/\n/g, '\\n').replace(/\{\{\{\s*(.+?)\s*\}\}\}/g, "{{!$1}}").split(/(\{\{\s*(.+?)\s*\}\})/g), 
+        var tokens = str.replace(/\n/g, '\\n').replace(/\{\{\{\s*(.+?)\s*\}\}\}/g, "{{!$1}}").split(/(\{\{\s*(.+?)\s*\}\})/g),
             i=0, toc, cmd, prop, val, fn, output = [];
 
         while(i < tokens.length) {
@@ -210,13 +210,13 @@
                 i = i + 1;
                 toc  = tokens[i];
                 cmd  = toc[0];
-                prop = toc.substring(toc.match(/^(\^|\#|\!|\~|\*)/) ? 1:0);
+                prop = toc.substring(toc.match(/^(\^|\#|\!|\~|\:)/) ? 1:0);
 
                 switch(cmd) {
                     case '~':
-                        output.push("for(var $index=0;$index<"+prop+".length;$index++) { var $item = "+prop+"[$index];");
+                        output.push("for(var $i=0;$i<"+prop+".length;$i++) { var $item = "+prop+"[$i];");
                         break;
-                    case '*':
+                    case ':':
                         output.push("for(var $key in "+prop+") { var $val = "+prop+"[$key];");
                         break;
                     case '#':
