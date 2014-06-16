bd422e03742f75168c229404142f2ecb20eefecd
Artur Heinze <artur@yootheme.com>
2014-06-16 10:21:12 +0200

    Fixes #434

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -15,7 +15,7 @@
             var $this = this;
 
             this.on("click", this.options.toggle, function(e) {
-                if ($(this).is('a[href="#"]')) e.preventDefault();
+                e.preventDefault();
                 $this.show(this);
             });
 
