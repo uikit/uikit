b2d51f331bd863ebfe771de664c35d574f773840
Artur Heinze <artur@yootheme.com>
2014-12-04 18:19:47 +0100

    version bump to 2.13.0

--- js/uikit.js
+++ js/uikit.js
@@ -1,4 +1,4 @@
-/*! UIkit 2.12.0 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
+/*! UIkit 2.13.0 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
 (function(core) {
 
     if (typeof define == "function" && define.amd) { // AMD
@@ -43,7 +43,7 @@
 
     var UI = {}, _UI = window.UIkit;
 
-    UI.version = '2.12.0';
+    UI.version = '2.13.0';
     UI._prefix = 'uk';
 
     UI.noConflict = function(prefix) {
