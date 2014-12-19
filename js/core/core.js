85ac68cabe16c6679c175fbd4c767bbc3a97ac84
Sascha Dube <sascha.dube@gmail.com>
2014-12-19 15:59:19 +0100

    updated to latest build

--- js/core/core.js
+++ js/core/core.js
@@ -1,4 +1,4 @@
-/*! UIkit 2.14.0 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
+/*! UIkit 2.15.0 | http://www.getuikit.com | (c) 2014 YOOtheme | MIT License */
 (function(core) {
 
     if (typeof define == "function" && define.amd) { // AMD
@@ -43,7 +43,7 @@
 
     var UI = {}, _UI = window.UIkit;
 
-    UI.version = '2.14.0';
+    UI.version = '2.15.0';
     UI._prefix = 'uk';
 
     UI.noConflict = function(prefix) {
