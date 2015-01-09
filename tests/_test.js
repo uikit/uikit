1b3a8f1b9dbc95d2fc05f48a958ff09f2a39dd43
Sascha Dube <sascha.dube@gmail.com>
2015-01-09 11:25:30 +0100

    updated accordion less

--- tests/_test.js
+++ tests/_test.js
@@ -12,6 +12,7 @@
 
         // components
         'dist/css/components/flex{style}.css',
+        'dist/css/components/accordion{style}.css',
         'dist/css/components/autocomplete{style}.css',
         'dist/css/components/caption{style}.css',
         'dist/css/components/cover{style}.css',
