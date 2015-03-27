62d26402b4bba47276d7df82cfbea47eb06869a3
Artur Heinze <artur@yootheme.com>
2015-03-27 18:15:25 +0100

    color animation added

--- src/js/components/parallax.js
+++ src/js/components/parallax.js
@@ -70,11 +70,24 @@
                     return;
                 }
 
-                var startend = String(this.options[prop]).split(','),
-                    start    = parseFloat(startend[1] ? startend[0] : this._getStartValue(prop)),
-                    end      = parseFloat(startend[1] ? startend[1] : startend[0]);
+                var start, end, dir, diff, startend = String(this.options[prop]).split(',');
 
-                this.props[prop] = { 'start': start, 'end': end, 'dir': (start < end ? 1:-1), 'diff': (start < end ? (end-start):(start-end)) };
+                if (prop.match(/color/i)) {
+                    start = startend[1] ? startend[0] : this._getStartValue(prop),
+                    end   = startend[1] ? startend[1] : startend[0];
+
+                    if (!start) {
+                        start = 'rgba(255,255,255,0)';
+                    }
+
+                } else {
+                    start = parseFloat(startend[1] ? startend[0] : this._getStartValue(prop)),
+                    end   = parseFloat(startend[1] ? startend[1] : startend[0]);
+                    diff  = (start < end ? (end-start):(start-end));
+                    dir   = (start < end ? 1:-1);
+                }
+
+                this.props[prop] = { 'start': start, 'end': end, 'dir': dir, 'diff': diff };
 
             }.bind(this));
 
@@ -116,7 +129,6 @@
             if (compercent < 0) compercent = 0;
             if (compercent > 1) compercent = 1;
 
-
             if (this._percent !== undefined && this._percent == compercent) {
                 return;
             }
@@ -129,7 +141,7 @@
                     val = opts.start;
                 } else if(percent === 1) {
                     val = opts.end;
-                } else {
+                } else if(opts.diff !== undefined) {
                     val = opts.start + (opts.diff * compercent * opts.dir);
                 }
 
@@ -169,6 +181,14 @@
                         css['background-position'] = '50% '+val+'%';
                         break;
 
+                    // color
+                    case "color":
+                    case "background-color":
+                    case "border-color":
+                        //console.log(opts)
+                        css[prop] = calcColor(opts.start, opts.end, compercent);
+                        break;
+
                     default:
                         css[prop] = val;
                         break;
@@ -194,6 +214,116 @@
         }
 
     });
+
+
+    // helper
+
+    // Some named colors to work with, added by Bradley Ayers
+    // From Interface by Stefan Petre
+    // http://interface.eyecon.ro/
+    var colors = {
+        'aqua': [0,255,255,1],
+        'azure': [240,255,255,1],
+        'beige': [245,245,220,1],
+        'black': [0,0,0,1],
+        'blue': [0,0,255,1],
+        'brown': [165,42,42,1],
+        'cyan': [0,255,255,1],
+        'darkblue': [0,0,139,1],
+        'darkcyan': [0,139,139,1],
+        'darkgrey': [169,169,169,1],
+        'darkgreen': [0,100,0,1],
+        'darkkhaki': [189,183,107,1],
+        'darkmagenta': [139,0,139,1],
+        'darkolivegreen': [85,107,47,1],
+        'darkorange': [255,140,0,1],
+        'darkorchid': [153,50,204,1],
+        'darkred': [139,0,0,1],
+        'darksalmon': [233,150,122,1],
+        'darkviolet': [148,0,211,1],
+        'fuchsia': [255,0,255,1],
+        'gold': [255,215,0,1],
+        'green': [0,128,0,1],
+        'indigo': [75,0,130,1],
+        'khaki': [240,230,140,1],
+        'lightblue': [173,216,230,1],
+        'lightcyan': [224,255,255,1],
+        'lightgreen': [144,238,144,1],
+        'lightgrey': [211,211,211,1],
+        'lightpink': [255,182,193,1],
+        'lightyellow': [255,255,224,1],
+        'lime': [0,255,0,1],
+        'magenta': [255,0,255,1],
+        'maroon': [128,0,0,1],
+        'navy': [0,0,128,1],
+        'olive': [128,128,0,1],
+        'orange': [255,165,0,1],
+        'pink': [255,192,203,1],
+        'purple': [128,0,128,1],
+        'violet': [128,0,128,1],
+        'red': [255,0,0,1],
+        'silver': [192,192,192,1],
+        'white': [255,255,255,1],
+        'yellow': [255,255,0,1],
+        'transparent': [255,255,255,0]
+    };
+
+    function calcColor(start, end, pos) {
+
+        /**!
+         * @preserve Color animation 1.6.0
+         * http://www.bitstorm.org/jquery/color-animation/
+         * Copyright 2011, 2013 Edwin Martin <edwin@bitstorm.org>
+         * Released under the MIT and GPL licenses.
+         */
+
+        start = parseColor(start);
+        end   = parseColor(end);
+        pos   = pos || 0;
+
+        //var properties = ['color', 'backgroundColor', 'borderBottomColor', 'borderLeftColor', 'borderRightColor', 'borderTopColor', 'outlineColor'];
+
+        // Calculate an in-between color. Returns "#aabbcc"-like string.
+        function calculateColor(begin, end, pos) {
+            var color = 'rgba('
+                    + parseInt((begin[0] + pos * (end[0] - begin[0])), 10) + ','
+                    + parseInt((begin[1] + pos * (end[1] - begin[1])), 10) + ','
+                    + parseInt((begin[2] + pos * (end[2] - begin[2])), 10) + ','
+                    + (begin && end ? parseFloat(begin[3] + pos * (end[3] - begin[3])) : 1);
+
+            color += ')';
+            return color;
+        }
+
+        // Parse an CSS-syntax color. Outputs an array [r, g, b]
+        function parseColor(color) {
+
+            var match, quadruplet;
+
+            // Match #aabbcc
+            if (match = /#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})/.exec(color)) {
+                quadruplet = [parseInt(match[1], 16), parseInt(match[2], 16), parseInt(match[3], 16), 1];
+
+                // Match #abc
+            } else if (match = /#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])/.exec(color)) {
+                quadruplet = [parseInt(match[1], 16) * 17, parseInt(match[2], 16) * 17, parseInt(match[3], 16) * 17, 1];
+
+                // Match rgb(n, n, n)
+            } else if (match = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(color)) {
+                quadruplet = [parseInt(match[1]), parseInt(match[2]), parseInt(match[3]), 1];
+
+            } else if (match = /rgba\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9\.]*)\s*\)/.exec(color)) {
+                quadruplet = [parseInt(match[1], 10), parseInt(match[2], 10), parseInt(match[3], 10),parseFloat(match[4])];
+
+                // No browser returns rgb(n%, n%, n%), so little reason to support this format.
+            } else {
+                quadruplet = colors[color] || [255,255,255,0];
+            }
+            return quadruplet;
+        }
+
+        return calculateColor(start, end, pos);
+    }
 
     return UI.parallax;
 });
