uikit/uikit

commit 4db3a9c82a6d47ec0d969ef5412d8fe6095b824b

fixed tooltip with empty title attributes

src/js/components/tooltip.js
@@ -76,7 +76,8 @@
 
             if (tooltipdelay)     clearTimeout(tooltipdelay);
             if (checkdelay)       clearTimeout(checkdelay);
-            if (!this.tip.length) return;
+
+            if (typeof(this.tip) === 'string' ? !this.tip.length:true) return;
 
             $tooltip.stop().css({"top": -2000, "visibility": "hidden"}).removeClass(this.options.activeClass).show();
             $tooltip.html('<div class="uk-tooltip-inner">' + this.tip + '</div>');

commit bc5edf73f9737a024bf0f24a4bbbcfb71545e0be

Fix error when tooltips with empty (but present) title, introduced in 4db3a9c. Also unnecessary argument `title` is not a local variable.

src/js/components/tooltip.js
@@ -27,11 +27,10 @@
             "delay": 0, // in miliseconds
             "cls": "",
             "activeClass": "uk-active",
-            "src": function(ele, title) {
+            "src": function(ele) {
+                var title = ele.attr('title');
 
-                title = ele.attr('title');
-
-                if (title) {
+                if (title !== undefined) {
                     ele.data('cached-title', title).removeAttr('title');
                 }
 