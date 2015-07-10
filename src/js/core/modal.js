e0b6c0293d83df755d66707070c6f24aaaef50b4
Artur Heinze <artur@yootheme.com>
2015-07-10 11:53:48 +0200

    cleanup

--- src/js/core/modal.js
+++ src/js/core/modal.js
@@ -284,7 +284,7 @@
 
         modal.on('show.uk.modal', function(){
             setTimeout(function(){
-                modal.element.find('button.js-modal-confirm').focus();
+                modal.element.find('.js-modal-confirm').focus();
             }, 50);
         });
 
