afe68ccd8336551b9c288160c4120b3658f829df
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-07-23 21:25:54 +0200

    Fix for tabs switching when `uk-tab > li > a` doesn't have any attributes at all

--- src/js/core/tab.js
+++ src/js/core/tab.js
@@ -147,7 +147,7 @@
 
                         if (!item.hasClass('uk-disabled')) {
 
-                            clone = item[0].outerHTML.replace('<a ', '<a data-index="'+i+'" ');
+                            clone = item[0].outerHTML.replace('<a', '<a data-index="'+i+'"');
 
                             this.responsivetab.lst.append(clone);
                         }
