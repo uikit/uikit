718ab6617017c864cf11f64141c68f5bf0a900ce
Tom Smedley <tom.s@msn.com>
2015-08-20 19:18:07 +0100

    uk-offcanvas-bar-over
    Modifier to allow a modern off-canvas bar that goes over content.

--- src/js/core/offcanvas.js
+++ src/js/core/offcanvas.js
@@ -18,6 +18,7 @@
                 bar       = element.find(".uk-offcanvas-bar:first"),
                 rtl       = (UI.langdirection == "right"),
                 flip      = bar.hasClass("uk-offcanvas-bar-flip") ? -1:1,
+                over      = bar.hasClass("uk-offcanvas-bar-over"),
                 dir       = flip * (rtl ? -1 : 1),
 
                 scrollbarwidth =  window.innerWidth - $body.width();
@@ -27,7 +28,7 @@
             element.addClass("uk-active");
 
             $body.css({"width": window.innerWidth - scrollbarwidth, "height": window.innerHeight}).addClass("uk-offcanvas-page");
-            $body.css((rtl ? "margin-right" : "margin-left"), (rtl ? -1 : 1) * (bar.outerWidth() * dir)).width(); // .width() - force redraw
+            if (!over) { $body.css((rtl ? "margin-right" : "margin-left"), (rtl ? -1 : 1) * (bar.outerWidth() * dir)).width(); // .width() - force redraw }
 
             $html.css('margin-top', scrollpos.y * -1);
 
