c1144e47283fa2e59a8691761a72e60c5bf43f58
Artur <artur@agentejo.com>
2016-01-08 00:56:30 +0100

    added support.js

--- src/js/uikit.js
+++ src/js/uikit.js
@@ -1,10 +1,12 @@
 import util from './core/util';
 import dom from './core/dom';
+import support from './core/support';
 
 
 var UI = {};
 
-UI.util = util;
-UI.dom  = dom;
+UI.util     = util;
+UI.dom      = dom;
+UI.support  = support;
 
 export default UI;
