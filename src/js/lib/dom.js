285173ecd7a7166782a1f4ec7e7f85d3f22b70fa
Artur Heinze <artur@yootheme.com>
2016-01-08 10:07:12 +0100

    cleanup

--- src/js/lib/dom.js
+++ src/js/lib/dom.js
@@ -3,16 +3,20 @@
 // small DOM pimping
 NodeList.prototype.forEach = NodeList.prototype.forEach || Array.prototype.forEach;
 
-let $DOM = window.jQuery;
+let $ = window.jQuery;
 
-$DOM.$doc  = $DOM(document);
-$DOM.$win  = $DOM(window);
-$DOM.$html = $DOM('html');
+$.$doc  = $(document);
+$.$win  = $(window);
+$.$html = $('html');
 
-$DOM.watch = function(el, fn, config) {
+$.watch = function(el, fn, config) {
     let observer = new MutationObserver(fn);
     observer.observe(el, config || { attributes: true, childList: true, characterData: true });
     return observer;
 };
 
-module.exports = $DOM;
+$.register = function(name, def) {
+    document.registerElement(name, { prototype: Object.assign(Object.create(HTMLElement.prototype), def || {}) });
+};
+
+module.exports = $;
