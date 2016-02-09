d57ce32d19c9956d92a4b249be2bb9111da5d5ad
janschoenherr <jan@yootheme.com>
2016-02-09 17:12:37 +0100

    fixed grid

--- src/js/core/grid.js
+++ src/js/core/grid.js
@@ -2,6 +2,8 @@ export default function (UIkit) {
 
     UIkit.component('grid', UIkit.components.marginWrap.extend({
 
+        name: 'grid',
+
         defaults: {
             margin: 'uk-grid-margin',
             rowFirst: 'uk-grid-first'
