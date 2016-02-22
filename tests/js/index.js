7d1adb37203424804f8f9175f3d3677456201074
janschoenherr <jan@yootheme.com>
2016-02-22 16:28:37 +0100

    fix

--- tests/js/index.js
+++ tests/js/index.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 require('../../vendor/holder.js');
 require('../../src/js/uikit.js');
 
-var base = '../' + document.querySelector('script[src*="test.js"]').getAttribute('src').replace('test.js', ''),
+var base = '../' + document.querySelector('script[src*="test.js"]').getAttribute('src').replace('js/test.js', ''),
 
     tests = {
 
