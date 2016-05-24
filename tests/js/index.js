d404b1c3bda21aacc5ba014fef15794d9812fdea
janschoenherr <jan@yootheme.com>
2016-05-24 17:26:53 +0200

    added selectbox for "themed" and "core" style to tests

--- tests/js/index.js
+++ tests/js/index.js
@@ -1,11 +1,20 @@
 import $ from 'jquery';
-module.exports = require('../../src/js/uikit.js');
 
 $('html').css('padding-top', '80px');
 
+var storage = window.sessionStorage, key = '_uikit_style';
+
+if (!storage[key]) {
+    storage[key] = '../css/uikit.css';
+}
+
+document.writeln(`<link rel="stylesheet" href="${storage[key]}">`);
+document.writeln(`<script src="../js/uikit.js"></script>`);
+
 requestAnimationFrame(function () {
 
-    var $select = $('<select><option value="">- Select Test -</option></select>').css('margin', '20px 0');
+    var $container = $('<div class="uk-form uk-container"></div>').prependTo('body');
+    var $tests = $('<select><option value="">- Select Test -</option></select>').css('margin', '20px 20px 20px 0').prependTo($container);
 
     [
         'accordion',
@@ -65,19 +74,30 @@ requestAnimationFrame(function () {
         'utility',
         'visibility',
         'width'
-    ].forEach(function (name) {
-        $('<option value="' + name + '.html">' + name.split('-').map(function (name) {
-                return name.charAt(0).toUpperCase() + name.slice(1);
-            }).join(' ') + '</option>').appendTo($select);
+    ].forEach(name => {
+        $(`<option value="${name}.html">${name.split('-').map(name => {
+            return name.charAt(0).toUpperCase() + name.slice(1);
+        }).join(' ')}</option>`).appendTo($tests);
     });
 
-    $select.on('change', function () {
-        if ($select.val()) {
-            location.href = '../' + document.querySelector('script[src*="test.js"]').getAttribute('src').replace('js/test.js', '') + 'tests/' + $select.val();
+    $tests.on('change', () => {
+        if ($tests.val()) {
+            location.href = `../${document.querySelector('script[src*="test.js"]').getAttribute('src').replace('js/test.js', '')}tests/${$tests.val()}`;
         }
     }).val(location.pathname.split('/').pop());
 
-    $('body').prepend($('<div class="uk-form uk-container"></div>').prepend($select));
+    var $styles = $(`
+        <select>
+            <option value="../css/uikit.css">Themed</option>
+            <option value="css/uikit.core.css">Core</option>
+        </select>
+    `).insertAfter($tests);
+
+    $styles.css('margin', '20px 0').on('change', () => {
+        storage[key] = $styles.val();
+        location.reload();
+    }).val(storage[key]);
+
     $('html').css('padding-top', '');
 
 });
