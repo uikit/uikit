5474ef203afdb0fd21ae207d8cd14a368e7139fc
Artur Heinze <artur@yootheme.com>
2016-07-27 11:53:45 +0200

    fixed rootpath for custom themes

--- build.js
+++ build.js
@@ -63,7 +63,7 @@ function buildthemes() {
         return fs.statSync(path.join('custom', file)).isDirectory();
     }).forEach(function(folder) {
         themes[folder] = `../css/uikit.${folder}.css`;
-        exec(`lessc --relative-urls --rootpath=custom/${folder}/ custom/${folder}/_import.less > css/uikit.${folder}.css`, function() {
+        exec(`lessc --relative-urls --rootpath=../custom/${folder}/ custom/${folder}/_import.less > css/uikit.${folder}.css`, function() {
             console.log(`${cyan('css/uikit.'+folder+'.css')} build`);
         });
     });
