0588cee1b3b0dcdbe0273e398daeaa064d101ca1
janschoenherr <jan@yootheme.com>
2016-08-04 10:31:41 +0200

    cleanup boot.js

--- src/js/api/boot.js
+++ src/js/api/boot.js
@@ -1,49 +1,46 @@
-import $ from 'jquery';
 import { camelize, matches, Observer, ready } from '../util/index';
 
 export default function (UIkit) {
 
     const DATA = UIkit.data;
 
-    if (!Observer) {
-        ready(() => $(UIkit.component.selector).each((i, node) => attachComponents(node)));
-        return;
-    }
+    if (Observer) {
 
-    (new Observer(mutations => {
+        (new Observer(mutations =>
 
-        mutations.forEach(mutation => {
-
-            if (mutation.type === 'childList') {
+            mutations.forEach(mutation => {
 
                 for (let i = 0; i < mutation.addedNodes.length; ++i) {
-                    apply(mutation.addedNodes[i], (node => {
-                        if (!node[DATA] && matches(node, UIkit.component.selector)) {
-                            attachComponents(node);
-                        }
-                    }));
+                    apply(mutation.addedNodes[i], attachComponents);
                 }
 
                 for (let i = 0; i < mutation.removedNodes.length; ++i) {
-                    apply(mutation.removedNodes[i], (node => {
-                        if (node[DATA]) {
-                            for (let name in node[DATA]) {
-                                node[DATA][name].$destroy();
-                            }
-                        }
-                    }));
+                    apply(mutation.removedNodes[i], removeComponents);
                 }
 
-            }
+            })
 
+        )).observe(document, {childList: true, subtree: true});
+
+    } else {
+
+        ready(() => {
+            apply(document.body, attachComponents);
+            document.body.addEventListener('DOMNodeInserted', e => apply(e.target, attachComponents));
+            document.body.addEventListener('DOMNodeRemoved', e => apply(e.target, removeComponents));
         });
 
-    })).observe(document, {childList: true, subtree: true});
+    }
 
     function attachComponents(node) {
-        for (var i = 0; i < node.attributes.length; i++) {
 
-            let name = node.attributes[i].name;
+        if (node[DATA] || !matches(node, UIkit.component.selector)) {
+            return;
+        }
+
+        for (var i = 0, name; i < node.attributes.length; i++) {
+
+            name = node.attributes[i].name;
 
             if (name.lastIndexOf('uk-', 0) === 0) {
                 name = camelize(name.replace('uk-', ''));
@@ -51,6 +48,14 @@ export default function (UIkit) {
                 if (UIkit[name]) {
                     UIkit[name](node);
                 }
+            }
+        }
+    }
+
+    function removeComponents(node) {
+        if (node[DATA]) {
+            for (let name in node[DATA]) {
+                node[DATA][name].$destroy();
             }
         }
     }
