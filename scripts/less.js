70dab101b388ef91d76cf3fd64a6a65ee5ed6680
Artur Heinze <artur@yootheme.com>
2016-08-11 17:39:38 +0200

    cleanup

--- scripts/less.js
+++ scripts/less.js
@@ -1,57 +1,31 @@
 var fs = require('fs');
 var exec = require('child_process').exec;
 var path = require('path');
-var uglify = require('uglify-js');
-var package = require('../package.json');
-var version = process.env.VERSION || package.version;
-var banner = `/*! UIkit ${version} | http://www.getuikit.com | (c) 2014 - 2016 YOOtheme | MIT License */\n`;
-
-buildthemes();
-
-function buildthemes() {
+var write = require('./util').write;
 
 
-    if (!fs.existsSync('themes.json') && !fs.existsSync('custom')) {
-        return write('themes.json', '{}');
-    }
+// build custom/*
+if (!fs.existsSync('themes.json') && !fs.existsSync('custom')) {
+    return write('themes.json', '{}');
+}
 
-    if (!fs.existsSync('custom')) {
-        return;
-    }
+if (!fs.existsSync('custom')) {
+    return;
+}
 
-    var themes = {};
+var themes = {};
 
-    fs.readdirSync('custom').filter(file => path.join('custom', file).match(/\.less$/)).forEach(theme => {
+fs.readdirSync('custom').filter(file => path.join('custom', file).match(/\.less$/)).forEach(theme => {
 
-        theme = path.basename(theme, '.less');
+    theme = path.basename(theme, '.less');
 
-        themes[theme] = {file: `../css/uikit.${theme}.css`};
+    themes[theme] = {file: `../css/uikit.${theme}.css`};
 
-        exec(`lessc --relative-urls --rootpath=../custom/ custom/${theme}.less > css/uikit.${theme}.css`, () => {
-            console.log(`${cyan('css/uikit.'+theme+'.css')} build`);
-        });
+    exec(`lessc --relative-urls --rootpath=../custom/ custom/${theme}.less > css/uikit.${theme}.css`, () => {
+        console.log(`${'css/uikit.'+theme+'.css'} build`);
     });
+});
 
-    if (Object.keys(themes).length) {
-        write('themes.json', JSON.stringify(themes));
-    }
-
-}
-
-function write(dest, code) {
-    return new Promise(function (resolve, reject) {
-        fs.writeFile(dest, code, function (err) {
-            if (err) return reject(err);
-            console.log(`${cyan(dest)} ${getSize(code)}`);
-            resolve();
-        });
-    });
-}
-
-function getSize(code) {
-    return `${(code.length / 1024).toFixed(2)}kb`;
-}
-
-function cyan(str) {
-    return `\x1b[1m\x1b[36m${str}\x1b[39m\x1b[22m`;
+if (Object.keys(themes).length) {
+    write('themes.json', JSON.stringify(themes));
 }
