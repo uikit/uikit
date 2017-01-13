d9374c3a521aa354a414aa6f622c0e6866ed4513
janschoenherr <jan@yootheme.com>
2017-01-13 14:36:55 +0100

    cleanup

--- build/release.js
+++ build/release.js
@@ -4,21 +4,14 @@ var util = require('./util');
 
 var info = require('../package.json');
 var file = `dist/uikit-${info.version}.zip`;
-var output = fs.createWriteStream(file);
+var output = fs.createWriteStream(file).on('close', () =>
+    console.log(`${util.cyan(file)} ${util.getSize(fs.readFileSync(file))}`)
+);
 
 var archive = archiver('zip');
 
-// listen for all archive data to be written
-output.on('close', function() {
-    console.log(`${util.cyan(file)} ${util.getSize(fs.readFileSync(file))}`);
-});
-
-// pipe archive data to the file
 archive.pipe(output);
-
 archive.file('dist/js/uikit.min.js', {name: '/js/uikit.min.js'});
 archive.file('dist/css/uikit.min.css', {name: '/css/uikit.min.css'});
 archive.directory('dist/images', '/images');
-
-// finalize the archive (ie we are done appending files but streams have to finish yet)
 archive.finalize();
