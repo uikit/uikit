ea0abe38b4865b640367ab605481eb6254a2be02
janschoenherr <jan@yootheme.com>
2017-02-24 15:07:09 +0100

    cleanup build scripts

--- build/less.js
+++ build/less.js
@@ -2,11 +2,16 @@ var fs = require('fs');
 var glob = require('glob');
 var path = require('path');
 var util = require('./util');
+var rtlcss = require('rtlcss');
+var postcss = require('postcss');
+var args = require('minimist')(process.argv)._;
+
+var rtl = ~args.indexOf('rtl');
 
 [
 
-    {src: 'src/less/uikit.less', dist: 'dist/css/uikit-core.css'},
-    {src: 'src/less/uikit.theme.less', dist: 'dist/css/uikit.css'}
+    {src: 'src/less/uikit.less', dist: `dist/css/uikit-core${rtl ? '.rtl' : ''}.css`},
+    {src: 'src/less/uikit.theme.less', dist: `dist/css/uikit${rtl ? '.rtl' : ''}.css`}
 
 ].forEach(config => compile(config.src, config.dist));
 
@@ -15,7 +20,7 @@ var themes = {};
 glob.sync('custom/*.less').forEach(file => {
 
     var theme = path.basename(file, '.less'),
-        dist = `dist/css/uikit.${theme}.css`;
+        dist = `dist/css/uikit.${theme}${rtl ? '.rtl' : ''}.css`;
 
     themes[theme] = {file: `../${dist}`};
 
@@ -23,7 +28,7 @@ glob.sync('custom/*.less').forEach(file => {
 
 });
 
-if (Object.keys(themes).length || !fs.existsSync('themes.json')) {
+if (!rtl && (Object.keys(themes).length || !fs.existsSync('themes.json'))) {
     util.write('themes.json', JSON.stringify(themes));
 }
 
@@ -32,9 +37,27 @@ function compile(file, dist) {
         util.renderLess(data, {
             relativeUrls: true,
             rootpath: '../../',
-            paths: ['custom/', 'src/less/']
+            paths: ['src/less/', 'custom/']
         })
             .then(util.makeRelative)
+            .then(output => !rtl && output || postcss([
+                css => {
+                    css.insertBefore(css.nodes[0], postcss.comment({text: 'rtl:begin:rename'}));
+                    css.insertAfter(css.nodes[css.nodes.length - 1], postcss.comment({text: 'rtl:end:rename'}));
+                },
+                rtlcss({
+                    stringMap: [{
+                        name: 'previous-next',
+                        priority: 100,
+                        search: ['previous', 'Previous', 'PREVIOUS'],
+                        replace: ['next', 'Next', 'NEXT'],
+                        options: {
+                            scope: '*',
+                            ignoreCase: false
+                        }
+                    }]
+                })
+            ]).process(output).css)
             .then(output => util.write(dist, output))
             .then(util.minify),
         error => console.log(error)
