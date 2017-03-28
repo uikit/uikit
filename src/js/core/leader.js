34d50c46d76cbb851e03b8a1c725f7aceb747781
Artur Heinze <artur@yootheme.com>
2017-03-28 16:00:00 +0200

    uk-leader refactored

--- src/js/core/leader.js
+++ src/js/core/leader.js
@@ -1,5 +1,5 @@
 import { Class } from '../mixin/index';
-import { $, getStyle } from '../util/index';
+import { $, getCssVar } from '../util/index';
 
 export default function (UIkit) {
 
@@ -8,47 +8,43 @@ export default function (UIkit) {
         mixins: [Class],
 
         connected() {
-
             this.filler = $('<span class="uk-leader-fill"></span>').appendTo(this.$el);
-            this.fillChar = getStyle(this.filler[0], 'content', ':after').replace(/^["'](.*)["']$/, '$1');
-
-            $(window).on('load resize', () => this.fill() );
-
-            this.fill();
+            this.fillChar = getCssVar('leader-fill');
         },
 
         disconnected() {
             this.filler.remove();
         },
 
-        methods: {
+        update: [
 
-            fill() {
+            {
+                write() {
 
-                var lw = this.$el.width();
-                var mw = this.filler.removeClass('uk-hidden').text('').width();
-                var oe = this.$el.offset().left;
-                var of = this.filler.offset().left;
-                var fill = of - oe;
-                var maxtimes = Math.floor(lw/mw);
-                var times = Math.floor((lw - fill)/mw);
+                    var lw = this.$el.width();
+                    var mw = this.filler.removeClass('uk-hidden').attr('data-fill', this.fillChar).width();
+                    var fill = this.filler.offset().left - this.$el.offset().left;;
+                    var maxtimes = Math.floor(lw / mw);
+                    var times = Math.floor((lw - fill) / mw);
 
-                // is filler alone in next row?
-                if (times/maxtimes > 0.9) {
-                    return this.filler.addClass('uk-hidden')
-                }
+                    if (times == 1 || times/maxtimes > 0.9) {
+                        this.filler.addClass('uk-hidden');
+                        return;
+                    }
 
-                var filltext = '';
+                    var filltext = '';
 
-                for (var i=0;i<times;i++) {
-                    filltext += this.fillChar;
-                }
+                    for (var i=0;i<times;i++) {
+                        filltext += this.fillChar;
+                    }
 
-                this.filler.text(filltext);
+                    this.filler.attr('data-fill', filltext);
+                },
+
+                events: ['load', 'resize']
+
             }
-
-        }
-
+        ]
     });
 
 }
