eb29eec88b33642ce1625c3593c34b7fe1b79a15
janschoenherr <jan@yootheme.com>
2017-04-19 09:31:29 +0200

    update build icons task

--- build/icons.js
+++ build/icons.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var glob = require('glob');
-var path = require('path');
 var util = require('./util');
+var shortid = require('shortid');
 var args = require('minimist')(process.argv);
 
 var custom = args.c || args.custom || 'custom/*/icons';
@@ -9,11 +9,15 @@ var match = args.n || args.name || '([a-z]+)/icons$';
 
 glob(custom, (err, folders) =>
     folders.forEach(folder => {
+
         var name = folder.toString().match(new RegExp(match, 'i'))[1];
-        util.write(`dist/${name}.json`, util.icons(`{src/images/icons,${folder}}/*.svg`)).then(file =>
-            util.compile('src/js/icons.js', `dist/js/uikit-icons-${name}`, [], {}, name, {icons: `dist/${name}`}).then(() =>
-                fs.unlink(`dist/${name}.json`, () => {})
+        var icons = `dist/${name}${shortid.generate()}`;
+
+        util.write(`${icons}.json`, util.icons(`{src/images/icons,${folder}}/*.svg`)).then(() =>
+            util.compile('src/js/icons.js', `dist/js/uikit-icons-${name}`, [], {}, name, {icons}).then(() =>
+                fs.unlink(`${icons}.json`, () => {})
             )
         )
+
     })
 );
