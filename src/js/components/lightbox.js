ae15bb95b6fc906115e7ac5004d77460cf85859c
janschoenherr <jan@yootheme.com>
2017-10-27 13:56:00 +0200

    Hide slidenav in Lightbox if single item only; #2947

--- src/js/components/lightbox.js
+++ src/js/components/lightbox.js
@@ -442,6 +442,8 @@ function plugin(UIkit) {
                 clearTimeout(this.controlsTimer);
                 this.controlsTimer = setTimeout(this.hideControls, this.delayControls);
 
+                attr($$(`[${this.attrItem}]`, this.$el), 'hidden', this.items.length < 2 ? '' : null);
+
                 addClass(this.$el, 'uk-active uk-transition-active');
 
             },
