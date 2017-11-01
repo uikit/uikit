6ef4e688ba9fe696839386637115043cc9235aad
janschoenherr <jan@yootheme.com>
2017-11-01 16:22:19 +0100

    update viewport height

--- src/js/core/height-viewport.js
+++ src/js/core/height-viewport.js
@@ -24,7 +24,7 @@ export default function (UIkit) {
 
                 css(this.$el, 'boxSizing', 'border-box');
 
-                var viewport = height(win), minHeight = 0, offsetTop = 0;
+                var viewport = height(win), minHeight, offsetTop = 0;
 
                 if (this.expand) {
 
@@ -63,15 +63,24 @@ export default function (UIkit) {
 
                     }
 
-                    minHeight = viewport - offsetTop;
+                    // on mobile devices (iOS and Android) window.innerHeight !== 100vh
+                    minHeight = offsetTop ? `calc(100vh - ${offsetTop}px)` : '100vh';
 
                 }
 
-                css(this.$el, 'minHeight', Math.max(minHeight, this.minHeight) || '');
+                if (!minHeight) {
+                    return;
+                }
+
+                css(this.$el, {height: '', minHeight});
+
+                var elHeight = this.$el.offsetHeight;
+                if (this.minHeight && this.minHeight > elHeight) {
+                    css(this.$el, 'minHeight', this.minHeight);
+                }
 
                 // IE 10-11 fix (min-height on a flex container won't apply to its flex items)
-                height(this.$el, '');
-                if (minHeight && viewport - offsetTop >= this.$el.offsetHeight) {
+                if (viewport - offsetTop >= elHeight) {
                     css(this.$el, 'height', minHeight);
                 }
 
