e0dca16f8ec5f47aa77840e13d25bdcd4224999e
Sascha Dube <sascha.dube@gmail.com>
2017-11-03 16:43:13 +0100

    cleanup cover component

--- src/js/core/cover.js
+++ src/js/core/cover.js
@@ -16,14 +16,6 @@ export default function (UIkit) {
             automute: true
         },
 
-        ready() {
-
-            if (this.$el.tagName === 'IFRAME') {
-                css(this.$el, 'pointerEvents', 'none');
-            }
-
-        },
-
         update: {
 
             write() {
