60f14905331e655a3a1644c53f211b35b11760f3
janschoenherr <jan@yootheme.com>
2017-12-12 17:17:35 +0100

    Lightbox will pass container prop to LightboxPanel

--- src/js/components/lightbox.js
+++ src/js/components/lightbox.js
@@ -18,6 +18,7 @@ function plugin(UIkit) {
 
         props: {
             animation: String,
+            container: Boolean,
             toggle: String,
             autoplay: Boolean,
             autoplayInterval: Number,
@@ -26,6 +27,7 @@ function plugin(UIkit) {
 
         defaults: {
             animation: undefined,
+            container: true,
             toggle: 'a',
             autoplay: 0,
             videoAutoplay: false
