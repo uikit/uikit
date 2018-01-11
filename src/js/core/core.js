0afda366dc9e68e3b5e8f55ced64e77fc1508a66
janschoenherr <jan@yootheme.com>
2018-01-11 12:03:12 +0100

    refactoring

---
+++ src/js/core/core.js
@@ -0,0 +1,52 @@
+import { $$, addClass, css, doc, hasTouch, on, ready, removeClass, toMs, win, within } from '../util/index';
+
+export default function (UIkit) {
+
+    ready(() => {
+
+        var scroll = 0, started = 0;
+
+        on(win, 'load resize', UIkit.update);
+        on(win, 'scroll', e => {
+            e.dir = scroll <= win.pageYOffset ? 'down' : 'up';
+            e.scrollY = scroll = win.pageYOffset;
+            UIkit.update(e);
+        });
+
+        on(doc, 'animationstart', ({target}) => {
+            if ((css(target, 'animationName') || '').match(/^uk-.*(left|right)/)) {
+                started++;
+                doc.body.style.overflowX = 'hidden';
+                setTimeout(() => {
+                    if (!--started) {
+                        doc.body.style.overflowX = '';
+                    }
+                }, toMs(css(target, 'animationDuration')) + 100);
+            }
+        }, true);
+
+        if (!hasTouch) {
+            return;
+        }
+
+        var cls = 'uk-hover';
+
+        on(doc, 'tap', ({target}) =>
+            $$(`.${cls}`).forEach((_, el) =>
+                !within(target, el) && removeClass(el, cls)
+            )
+        );
+
+        Object.defineProperty(UIkit, 'hoverSelector', {
+
+            set(selector) {
+                on(doc, 'tap', selector, ({current}) => addClass(current, cls));
+            }
+
+        });
+
+        UIkit.hoverSelector = '.uk-animation-toggle, .uk-transition-toggle, [uk-hover]';
+
+    });
+
+}
