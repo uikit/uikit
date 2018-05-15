7d2f8911753a53e1b6f77c0706883330e8c2095a
Aaron Gerig <a.gerig@w-vision.ch>
2018-05-15 18:10:14 +0200

    Fix error message for maxSize option

--- src/js/components/upload.js
+++ src/js/components/upload.js
@@ -28,7 +28,7 @@ export default {
         mime: false,
         msgInvalidMime: 'Invalid File Type: %s',
         msgInvalidName: 'Invalid File Name: %s',
-        msgInvalidSize: 'Invalid File Size: %s Bytes Max',
+        msgInvalidSize: 'Invalid File Size: %s Kilobytes Max',
         multiple: false,
         name: 'files[]',
         params: {},
@@ -107,7 +107,7 @@ export default {
             for (let i = 0; i < files.length; i++) {
 
                 if (this.maxSize && this.maxSize * 1000 < files[i].size) {
-                    this.fail(this.msgInvalidSize.replace('%s', this.allow));
+                    this.fail(this.msgInvalidSize.replace('%s', this.maxSize));
                     return;
                 }
 
