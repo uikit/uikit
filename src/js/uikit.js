7fcf09326d8251a5766986f7af696e5051f6249e
Juan Lago <juanparati@gmail.com>
2020-04-28 14:38:56 +0200

    Adapted to 3.3.x

--- src/js/uikit.js
+++ src/js/uikit.js
@@ -1,36 +1,21 @@
+import UIkit from './api/index';
+import Core from './core/core';
 import boot from './api/boot';
-import UIkit from './uikit-core';
-import Countdown from './components/countdown';
-import Filter from './components/filter';
-import Lightbox from './components/lightbox';
-import lightboxPanel from './components/lightbox-panel';
-import Notification from './components/notification';
-import Parallax from './components/parallax';
-import Slider from './components/slider';
-import SliderParallax from './components/slider-parallax';
-import Slideshow from './components/slideshow';
-import Sortable from './components/sortable';
-import TableScrollable from './components/table-scrollable';
-import Tooltip from './components/tooltip';
-import Upload from './components/upload';
+import * as coreComponents from './core/index';
+import * as components from './components/index';
+import {each} from './util/lang';
 
-UIkit.component('countdown', Countdown);
-UIkit.component('filter', Filter);
-UIkit.component('lightbox', Lightbox);
-UIkit.component('lightboxPanel', lightboxPanel);
-UIkit.component('notification', Notification);
-UIkit.component('parallax', Parallax);
-UIkit.component('slider', Slider);
-UIkit.component('sliderParallax', SliderParallax);
-UIkit.component('slideshow', Slideshow);
-UIkit.component('slideshowParallax', SliderParallax);
-UIkit.component('sortable', Sortable);
-UIkit.component('tableScrollable', TableScrollable);
-UIkit.component('tooltip', Tooltip);
-UIkit.component('upload', Upload);
+// register components
+each(coreComponents, register);
+each(components, register);
 
-if (BUNDLED) {
-    boot(UIkit);
-}
+// core functionality
+UIkit.use(Core);
+
+boot(UIkit);
 
 export default UIkit;
+
+function register(component, name) {
+    UIkit.component(name, component);
+}
