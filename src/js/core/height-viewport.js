98b9c1e7dc0bfa3b7cf09d5c43a3a1252b1739fb
janschoenherr <jan@yootheme.com>
2022-05-04 15:43:18 +0200

    revert: Height Viewport component sets `uk-height-viewport` class to reduce initial layout shifts (conflicts with setting sticky on first section with offset-top: true)

--- src/js/core/height-viewport.js
+++ src/js/core/height-viewport.js
@@ -1,4 +1,3 @@
-import Class from '../mixin/class';
 import Resize from '../mixin/resize';
 import {
     boxModelAdjust,
@@ -15,7 +14,7 @@ import {
 } from 'uikit-util';
 
 export default {
-    mixins: [Class, Resize],
+    mixins: [Resize],
 
     props: {
         expand: Boolean,
