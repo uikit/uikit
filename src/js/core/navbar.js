60d9b3eca8b403614ab9e8ba469d171a128e11e1
janschoenherr <jan@yootheme.com>
2022-07-04 11:39:51 +0200

    refactor: add `uk-dropbar` class to dropbar

--- src/js/core/navbar.js
+++ src/js/core/navbar.js
@@ -105,7 +105,7 @@ export default {
             },
 
             watch(dropbar) {
-                addClass(dropbar, 'uk-navbar-dropbar');
+                addClass(dropbar, 'uk-navbar-dropbar', 'uk-dropbar');
             },
 
             immediate: true,
