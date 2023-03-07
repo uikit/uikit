42a6be914c82e52306d833821e9ea1868f3de28e
janschoenherr <jan@yootheme.com>
2023-03-07 12:06:54 +0100

    chore: remove timestamp

--- src/js/api/log.js
+++ src/js/api/log.js
@@ -1,15 +1,10 @@
 export function log(instance, eventName, eventData) {
     console.log(
-        `🔔 %s%s:%d %c%s`,
-        createTimestamp(),
+        `🔔 %s:%d %c%s`,
         instance.$options.name,
         instance._uid,
         'font-weight:bold;',
         eventName,
         [...(eventData ?? []), instance, instance.$el]
     );
-}
-
-function createTimestamp(time = new Date()) {
-    return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}.${time.getMilliseconds()} `;
 }
