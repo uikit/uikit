753219e668311aa03e603f69ba10d38824cc9d41
janschoenherr <jan@yootheme.com>
2023-12-18 11:26:15 +0100

    refactor: remove fallback for aspect-ratio

--- src/js/components/slideshow.js
+++ src/js/components/slideshow.js
@@ -1,12 +1,10 @@
-import { boxModelAdjust, css, dimensions, inBrowser } from 'uikit-util';
+import { css } from 'uikit-util';
 import Class from '../mixin/class';
 import SliderParallax from '../mixin/slider-parallax';
 import SliderReactive from '../mixin/slider-reactive';
 import Slideshow from '../mixin/slideshow';
 import SliderPreload from './internal/slider-preload';
 import Animations from './internal/slideshow-animations';
-
-const supportsAspectRatio = inBrowser && CSS.supports('aspect-ratio', '1/1');
 
 export default {
     mixins: [Class, Slideshow, SliderReactive, SliderParallax, SliderPreload],
@@ -27,62 +25,21 @@ export default {
         Animations,
     },
 
-    computed: {
-        totalWidth() {
-            return dimensions(this.list).width * this.length;
-        },
-    },
-
     watch: {
         list(list) {
-            if (list && supportsAspectRatio) {
-                css(list, {
-                    aspectRatio: this.ratio.replace(':', '/'),
-                    minHeight: this.minHeight || '',
-                    maxHeight: this.maxHeight || '',
-                    minWidth: '100%',
-                    maxWidth: '100%',
-                });
-            }
+            css(list, {
+                aspectRatio: this.ratio.replace(':', '/'),
+                minHeight: this.minHeight || '',
+                maxHeight: this.maxHeight || '',
+                minWidth: '100%',
+                maxWidth: '100%',
+            });
         },
-    },
-
-    update: {
-        // deprecated: Remove with iOS 17
-        read() {
-            if (!this.list || supportsAspectRatio) {
-                return false;
-            }
-
-            let [width, height] = this.ratio.split(':').map(Number);
-
-            height = (height * this.list.offsetWidth) / width || 0;
-
-            if (this.minHeight) {
-                height = Math.max(this.minHeight, height);
-            }
-
-            if (this.maxHeight) {
-                height = Math.min(this.maxHeight, height);
-            }
-
-            return { height: height - boxModelAdjust(this.list, 'height', 'content-box') };
-        },
-
-        write({ height }) {
-            height > 0 && css(this.list, 'minHeight', height);
-        },
-
-        events: ['resize'],
     },
 
     methods: {
         getAdjacentSlides() {
             return [1, -1].map((i) => this.slides[this.getIndex(this.index + i)]);
-        },
-
-        getSlideWidthAt() {
-            return dimensions(this.list).width;
         },
     },
 };
--- src/js/core/responsive.js
+++ src/js/core/responsive.js
@@ -1,49 +1,10 @@
-import { addClass, css, Dimensions, height, inBrowser, isVisible, parent, width } from 'uikit-util';
-import { resize } from '../api/observables';
-
-const supportsAspectRatio = inBrowser && CSS.supports('aspect-ratio', 1);
+import { addClass, css } from 'uikit-util';
 
 export default {
-    mixins: [
-        supportsAspectRatio
-            ? {
-                  connected() {
-                      css(this.$el, 'aspectRatio', `${this.width}/${this.height}`);
-                  },
-              }
-            : {
-                  observe: resize({
-                      target: ({ $el }) => [$el, parent($el)],
-                  }),
-
-                  update: {
-                      read() {
-                          return isVisible(this.$el) && this.width && this.height
-                              ? { width: width(parent(this.$el)), height: this.height }
-                              : false;
-                      },
-
-                      write(dim) {
-                          height(
-                              this.$el,
-                              Dimensions.contain(
-                                  {
-                                      height: this.height,
-                                      width: this.width,
-                                  },
-                                  dim,
-                              ).height,
-                          );
-                      },
-
-                      events: ['resize'],
-                  },
-              },
-    ],
-
     props: ['width', 'height'],
 
     connected() {
         addClass(this.$el, 'uk-responsive-width');
+        css(this.$el, 'aspectRatio', `${this.width}/${this.height}`);
     },
 };
