1a3d20074a210cc09cbd19a442b7ed61a49cc5a7
janschoenherr <jan@yootheme.com>
2024-04-02 16:59:50 +0200

    feat: Add video hover mode

--- src/js/core/video.js
+++ src/js/core/video.js
@@ -1,4 +1,16 @@
-import { hasAttr, isTag, isVideo, mute, parent, pause, play } from 'uikit-util';
+import {
+    hasAttr,
+    includes,
+    isTag,
+    isTouch,
+    isVideo,
+    mute,
+    parent,
+    pause,
+    play,
+    pointerEnter,
+    pointerLeave,
+} from 'uikit-util';
 import { intersection } from '../api/observables';
 
 export default {
@@ -14,7 +26,7 @@ export default {
         autoplay: true,
     },
 
-    connected() {
+    beforeConnect() {
         if (this.autoplay === 'inview' && !hasAttr(this.$el, 'preload')) {
             this.$el.preload = 'none';
         }
@@ -23,14 +35,50 @@ export default {
             this.$el.allow = 'autoplay';
         }
 
+        if (this.autoplay === 'hover') {
+            if (isTag(this.$el, 'video')) {
+                this.$el.tabindex = 0;
+            } else {
+                this.autoplay = true;
+            }
+        }
+
         if (this.automute) {
             mute(this.$el);
         }
     },
 
+    events: [
+        {
+            name: `${pointerEnter} focusin`,
+            filter() {
+                return includes(this.autoplay, 'hover');
+            },
+            handler(e) {
+                if (!isTouch(e) || !isPlaying(this.$el)) {
+                    play(this.$el);
+                } else {
+                    pause(this.$el);
+                }
+            },
+        },
+
+        {
+            name: `${pointerLeave} focusout`,
+            filter() {
+                return includes(this.autoplay, 'hover');
+            },
+            handler(e) {
+                if (!isTouch(e)) {
+                    pause(this.$el);
+                }
+            },
+        },
+    ],
+
     observe: [
         intersection({
-            filter: ({ $el, autoplay }) => autoplay && isVideo($el),
+            filter: ({ $el, autoplay }) => autoplay && autoplay !== 'hover' && isVideo($el),
             handler([{ isIntersecting }]) {
                 if (!document.fullscreenElement) {
                     if (isIntersecting) {
@@ -45,3 +93,7 @@ export default {
         }),
     ],
 };
+
+function isPlaying(videoEl) {
+    return !videoEl.paused && !videoEl.ended;
+}
