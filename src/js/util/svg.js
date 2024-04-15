c6a7fbca6a16ccff1ff2f7697cb0adbda5830235
janschoenherr <jan@yootheme.com>
2024-04-15 11:27:39 +0200

    refactor: cleanup

--- src/js/util/svg.js
+++ src/js/util/svg.js
@@ -1,16 +1,5 @@
 import { $$ } from './dom';
 
 export function getMaxPathLength(el) {
-    return Math.ceil(
-        Math.max(
-            0,
-            ...$$('[stroke]', el).map((stroke) => {
-                try {
-                    return stroke.getTotalLength();
-                } catch (e) {
-                    return 0;
-                }
-            }),
-        ),
-    );
+    return Math.ceil(Math.max(0, ...$$('[stroke]', el).map((stroke) => stroke.getTotalLength?.())));
 }
