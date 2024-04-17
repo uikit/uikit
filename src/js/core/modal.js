e37e467983eda6d34a1f23a17d43483404e272fd
janschoenherr <jan@yootheme.com>
2024-04-17 14:06:25 +0200

    fix: dialogs

--- src/js/core/modal.js
+++ src/js/core/modal.js
@@ -70,12 +70,11 @@ export default {
 
 function install({ modal }) {
     modal.dialog = function (content, options) {
-        const dialog = modal(
-            `<div class="uk-modal">
-                <div class="uk-modal-dialog">${content}</div>
-             </div>`,
-            { stack: true, role: 'alertdialog', ...options },
-        );
+        const dialog = modal($(`<div><div class="uk-modal-dialog">${content}</div></div>`), {
+            stack: true,
+            role: 'alertdialog',
+            ...options,
+        });
 
         dialog.show();
 
