76ecfef9e8935d354f76d2829687dd6d0a17c4a2
janschoenherr <jan@yootheme.com>
2024-05-06 16:44:55 +0200

    fix: check if elementData still exists

--- src/js/core/scrollspy.js
+++ src/js/core/scrollspy.js
@@ -108,7 +108,7 @@ export default {
 
     methods: {
         toggle(el, inview) {
-            const state = this.elementData.get(el);
+            const state = this.elementData?.get(el);
 
             if (!state) {
                 return;
