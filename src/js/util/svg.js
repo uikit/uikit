6f62fc2a091fb71ae47bb61f5eeb38825f906db6
janschoenherr <jan@yootheme.com>
2024-06-05 17:20:42 +0200

    fix: getTotalLength is only available on SVGGeometryElement

--- src/js/util/svg.js
+++ src/js/util/svg.js
@@ -4,7 +4,7 @@ import { isVisible } from './filter.js';
 export function getMaxPathLength(el) {
     return isVisible(el)
         ? Math.ceil(
-              Math.max(0, ...$$('[stroke]', el).map((stroke) => stroke?.getTotalLength() || 0)),
+              Math.max(0, ...$$('[stroke]', el).map((stroke) => stroke.getTotalLength?.() || 0)),
           )
         : 0;
 }
