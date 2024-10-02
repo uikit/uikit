e06640359722b71dc33e5979ef6efa0d54a64630
janschoenherr <jan@yootheme.com>
2024-10-02 08:41:07 +0200

    refactor: events can no longer be registered by strings

--- src/js/api/events.js
+++ src/js/api/events.js
@@ -1,4 +1,4 @@
-import { hasOwn, isArray, isFunction, isPlainObject, isString, on } from 'uikit-util';
+import { hasOwn, isArray, isPlainObject, on } from 'uikit-util';
 
 export function initEvents(instance) {
     for (const event of instance.$options.events || []) {
@@ -16,19 +16,17 @@ export function registerEvent(instance, event, key) {
     let { name, el, handler, capture, passive, delegate, filter, self } = isPlainObject(event)
         ? event
         : { name: key, handler: event };
-    el = isFunction(el) ? el.call(instance, instance) : el || instance.$el;
+    el = el ? el.call(instance, instance) : instance.$el;
 
     if (!el || (isArray(el) && !el.length) || (filter && !filter.call(instance, instance))) {
         return;
     }
 
     instance._disconnect.push(
-        on(
-            el,
-            name,
-            delegate ? (isString(delegate) ? delegate : delegate.call(instance, instance)) : null,
-            isString(handler) ? instance[handler] : handler.bind(instance),
-            { passive, capture, self },
-        ),
+        on(el, name, delegate?.call(instance, instance), handler.bind(instance), {
+            passive,
+            capture,
+            self,
+        }),
     );
 }
