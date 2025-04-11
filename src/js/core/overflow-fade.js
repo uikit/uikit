542e17ad63e0fb17598e0f784a96f3c797a3f8a3
janschoenherr <jan@yootheme.com>
2025-04-11 11:32:39 +0200

    feat: add Overflow Fade component (JavaScript)

---
+++ src/js/core/overflow-fade.js
@@ -0,0 +1,52 @@
+import { addClass, clamp, css } from 'uikit-util';
+import { resize } from '../api/observables';
+
+export default {
+    props: {
+        axis: String,
+    },
+
+    data: {
+        axis: 'x',
+        margin: 0.2,
+    },
+
+    connected() {
+        addClass(this.$el, `${this.$options.id}-${this.axis === 'x' ? 'horizontal' : 'vertical'}`);
+    },
+
+    events: {
+        name: 'scroll',
+
+        self: true,
+
+        passive: true,
+
+        handler() {
+            this.$emit();
+        },
+    },
+
+    observe: [resize()],
+
+    update: {
+        read() {
+            const prop = this.axis === 'x' ? 'Width' : 'Height';
+            return { overflow: this.$el[`scroll${prop}`] - this.$el[`client${prop}`] };
+        },
+
+        write({ overflow }) {
+            const percent = overflow
+                ? this.$el[`scroll${this.axis === 'x' ? 'Left' : 'Top'}`] / overflow
+                : 0;
+            const toValue = (value) => (overflow ? clamp((this.margin - value) / this.margin) : 0);
+
+            css(this.$el, {
+                '--uk-overflow-fade-start-opacity': toValue(percent),
+                '--uk-overflow-fade-end-opacity': toValue(1 - percent),
+            });
+        },
+
+        events: ['resize'],
+    },
+};
