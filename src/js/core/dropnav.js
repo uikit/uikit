13a98c670b0c12712af23bce5dfb7c223c97df7e
janschoenherr <jan@yootheme.com>
2025-05-27 16:56:18 +0200

    fix: prevent initial pointer events from opening dropdowns on page load in Dropnav component (in Safari and Firefox)

--- src/js/core/dropnav.js
+++ src/js/core/dropnav.js
@@ -15,8 +15,12 @@ import {
     noop,
     observeResize,
     offset,
+    on,
     once,
     parents,
+    pointerEnter,
+    pointerLeave,
+    pointerMove,
     query,
     remove,
     selFocusable,
@@ -119,6 +123,8 @@ export default {
 
     connected() {
         this.initializeDropdowns();
+
+        preventInitialPointerEnter(this.$el);
     },
 
     disconnected() {
@@ -429,4 +435,14 @@ function handleNavItemNavigation(e, toggles, active) {
         active.hide?.(false);
         toggles[getIndex(next, toggles, toggles.indexOf(active.targetEl || current))].focus();
     }
+}
+
+// Prevents initial pointer events from opening dropdowns on page load (Safari/Firefox)
+function preventInitialPointerEnter(el) {
+    const off = () => handlers.forEach((handler) => handler());
+    const handlers = [
+        once(el.ownerDocument, pointerMove, (e) => el.contains(e.target) || off()),
+        on(el, `mouseenter ${pointerEnter}`, (e) => e.stopPropagation(), { capture: true }),
+        on(el, `mouseleave ${pointerLeave}`, off, { capture: true }),
+    ];
 }
