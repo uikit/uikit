bed93b91a5bbb0b0c37189fafb9e99840a1ac5f4
janschoenherr <jan@yootheme.com>
2026-02-10 10:50:02 +0100

    feat: add Marquee JS component

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,6 +2,7 @@ export { default as Countdown } from './countdown';
 export { default as Filter } from './filter';
 export { default as Lightbox } from './lightbox';
 export { default as LightboxPanel } from './lightbox-panel';
+export { default as Marquee } from './marquee';
 export { default as Notification } from './notification';
 export { default as Parallax } from './parallax';
 export { default as Slider } from './slider';
